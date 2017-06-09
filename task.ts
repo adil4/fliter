cb495510cdeacf75e7df2e27852e3944e3a4ddae
adil4 <adilismail4@gmail.com>
2017-06-09 05:33:15 -0700

    adil

---
+++ task.ts
@@ -0,0 +1,69 @@
+let adults = [
+    {
+        name:'humza',
+        age:10,
+    },
+    {
+        name:'ali',
+        age:12,
+    },
+    {
+        name:'hiba',
+        age:14,
+    },
+    {
+        name:'dayan',
+        age:16,
+    },
+    {
+        name:'farzam',
+        age:18,
+    },
+    {
+        name:'ammar',
+        age:20,
+    },
+    {
+        name:'habib',
+        age:22,
+    },
+    {
+        name:'adil',
+        age:24,
+    },
+    {
+        name:'zubair',
+        age:26,
+    },
+    {
+        name:'ali',
+        age:28,
+    },
+    {
+        name:'fahad',
+        age:30,
+    },
+    {
+        name:'ashar',
+        age:35,
+    },
+    {
+        name:'umair',
+        age:40,
+    },
+    {
+        name:'noor',
+        age:45,
+    },
+    {
+        name:'wasif',
+        age:50,
+    },
+    
+]
+console.log(adults);
+
+let arr = adults.filter(function(obj){
+    return (obj['age'] < 20 || obj.age > 30);
+});
+console.log('filtered values', arr);
